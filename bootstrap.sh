e4e173ab899ff7fc0835dc253e33d80092eed333
prdobby <twoplasticforks@gmail.com>
2018-06-09 16:40:26 -0400

    Added initial git dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Bootstrapping dotfiles"
+
+source git/bootstrap.sh
---
+++ git/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Bootstrapping git dotfiles"
+
+SCRIPT_DIR="${0%/*}"
+
+source $SCRIPT_DIR/git/git-completion.sh
+source $SCRIPT_DIR/git/git-prompt.sh
+
+ln -s $SCRIPT_DIR/git/.gitconfig $HOME/.gitconfig
+
