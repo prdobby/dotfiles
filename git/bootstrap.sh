125eeb21c2d5d056667d3ea63ba3b8a2eca061a7
prdobby <twoplasticforks@gmail.com>
2018-06-09 16:57:39 -0400

    Moved gitconfig so that it's visible in the repo

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -7,5 +7,5 @@ SCRIPT_DIR="${0%/*}"
 source $SCRIPT_DIR/git/git-completion.sh
 source $SCRIPT_DIR/git/git-prompt.sh
 
-ln -s $SCRIPT_DIR/git/.gitconfig $HOME/.gitconfig
+ln -s $SCRIPT_DIR/git/gitconfig $HOME/.gitconfig
 
