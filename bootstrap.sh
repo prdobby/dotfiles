81ab199a461e412774cbcbdebf0a770dd119cb92
prdobby <twoplasticforks@gmail.com>
2018-08-12 07:58:28 -0400

    Updating bootstrap files to fix paths and symlinking

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,8 @@
 
 echo "Bootstrapping dotfiles"
 
-source git/bootstrap.sh
-source vim/bootstrap.sh
+SCRIPT_SOURCE=${BASH_SOURCE[0]}
+DIR_NAME=`dirname ${BASH_SOURCE[0]}`
+
+source $DIR_NAME/git/bootstrap.sh
+source $DIR_NAME/vim/bootstrap.sh
